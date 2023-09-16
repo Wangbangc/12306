ae1ce6c22157e7b29f5ea7a1dcb68944753e1d4e
jaryarbn <grandline501@gmail.com>
2023-09-16 11:37:07 +0800

    update readme

---
+++ src/main/java/com/hrbnu/demo12306/springboot12306/Application.java
@@ -0,0 +1,13 @@
+package com.hrbnu.demo12306.springboot12306;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/hrbnu/demo12306/springboot12306/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hrbnu.demo12306.springboot12306;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
