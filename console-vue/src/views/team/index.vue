b9c074d61e18c27748784ec43f68e1b8e4669881
Wangbangc <747880962@qq.com>
2023-09-21 16:45:38 +0800

    前端项目搭建

---
+++ console-vue/src/views/team/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <a-card title="第9组" style="width: 300px">
+    <template #extra><a href="#">team</a></template>
+    <p>姜塬滨，田嘉怡</p>
+    <p>张梓褀，王邦城</p>
+    <p>郑鑫，衣红竹</p>
+  </a-card>
+</template>
